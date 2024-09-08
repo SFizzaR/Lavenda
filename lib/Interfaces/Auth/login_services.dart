b68a292d943c5107c3db47d9c2cb6edcf2ddfa67
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2024-09-08 23:12:01 +0500

    Add files via upload

---
+++ lib/Interfaces/Auth/login_services.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginServices {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+}
